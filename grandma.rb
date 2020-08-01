d9cca2bb276de233044eacc72e192d4f269c7e79
Danniely Santos <dannielyingrid@gmail.com>
2020-08-01 21:39:08 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,10 +3,10 @@
 def speak_to_grandma(phrase)
   if prhase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elsif phrase
-    return "HUH?! SPEAK UP, SUNNY!"
-  else prhase == phrase.upcase
+  elsif prhase == phrase.upcase
     return "NO, NOT SINCE 1938!"
+  else
+    return "HUH?! SPEAK UP, SUNNY!"
   end
 end
 
